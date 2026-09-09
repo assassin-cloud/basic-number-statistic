64adfe8c43df586f604844127c68c425fb76a8bd
Assassin Spider <raif75219@gmail.com>
2026-09-09 17:10:19 +0500

    Refactor number analyzer for improved usability

--- numberanalyzer.cpp
+++ numberanalyzer.cpp
@@ -8,75 +8,77 @@ void welcome(){
     cout << endl;
 }
 
-void array(int size){
-    double *p = new double[size];
-    cout << "Enter " << size << " numbers: " << endl;
-    for (int i=0;i<size;i++){
-        cin >> p[i];
-    }
+void array(){
+    int size;
+    cout << "How many number do you want to analyze: ";
+    cin >> size;
     if(cin.fail()){
         cout << "Invalid input" << endl;
         cin.clear();
         cin.ignore(1000, '\n');
-        delete[] p;
-        p = nullptr;
     }
     else{
-        cout << "Array you created: " << endl;
-        for(int i=0;i<size;i++){
-            cout << p[i] << " , ";
+        if(size <= 0){
+            cout << "Invalid array size" << endl;
         }
-        cout << endl;
-        double largest = p[0];
-        for(int i=0;i<size;i++){
-            if(p[i] > largest){
-                largest = p[i];
+        else{
+            double *p = new double[size];
+            cout << "Enter " << size << " numbers: " << endl;
+            for (int i=0;i<size;i++){
+                cin >> p[i];
+            }
+
+            if(cin.fail()){
+                cout << "Invalid input" << endl;
+                cin.clear();
+                cin.ignore(1000, '\n');
+                delete[] p;
+                p = nullptr;
+            }
+
+            else{
+                cout << "Array you created: " << endl;
+                for(int i=0;i<size;i++){
+                cout << p[i] << " , ";
+                }
+                cout << endl;
+                double largest = p[0];
+                double smallest = p[0];
+                double sum = 0;
+                for(int i=0;i<size;i++){
+                    if(p[i] > largest){
+                        largest = p[i];
+                    }
+                    if(p[i] < smallest){
+                        smallest = p[i];
+                    }
+                    sum += p[i];
+                }
+                cout << "Largest number: " << largest << endl;
+                cout << "Smallest number: " << smallest << endl;
+                cout << "Sum: " << sum << endl;
+                cout << "Average: " << sum/size;
+                cout << endl;
+                delete[] p;
+                p = nullptr;
             }
         }
-        cout << "Largest number: " << largest << endl;
-
-        double smallest = p[0];
-        for(int i=0;i<size;i++){
-            if(p[i] < smallest){
-                smallest = p[i];
-            }
-        }
-        cout << "Smallest number: " << smallest << endl;
-
-        double sum = 0;
-        for(int i=0;i<size;i++){
-            sum += p[i];
-        }
-        cout << "Sum: " << sum << endl;
-        cout << "Average: " << sum/size;
-        cout << endl;
-        delete[] p;
-        p = nullptr;
     }
 }
 
     
 int main(){
-    int size;
+    string programend;
     cout << endl;
     welcome();
     while(true){
-        cout << "How many number do you want to analyze: ";
-        cin >> size;
-        if(cin.fail()){
-            cout << "Invalid input" << endl;
-            cin.clear();
-            cin.ignore(1000, '\n');
+        cout << "Type y to run or anything to quit" << endl;
+        cin >> programend;
+        if(programend == "y"){
+            array();
         }
         else{
-            if(size <= 0){
-                cout << "Invalid array size" << endl;
-            }
-            else{
-                array(size);
-                cout << endl;
-            }
+            break;
         }
     }
 }
-
